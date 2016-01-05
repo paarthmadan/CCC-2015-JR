6b334fa61d6f39f6e6c13d76ef0caba074338e20
paarthmadan <madanhockey@hotmail.com>
2016-01-04 20:25:35 -0500

    commented files

--- src/J1.java
+++ src/J1.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class J1 {
 	public static void main(String [] args){
+		
+		//intializing Scanner object
 		Scanner input = new Scanner(System.in);
 		
+		//intializing variables
 		int month = input.nextInt();
 		int day = input.nextInt();
 		
+		//basic if statement deducing whether input is before or after certain integers
 		if(month == 1 && day < 18){
 			System.out.println("Before");
 		}else if(month == 1 && day == 18){
--- src/J2.java
+++ src/J2.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 public class J2 {
 	public static void main(String [] args){
+		
+		//initializing Scanner object
 		Scanner input = new Scanner(System.in);
 		
+		//initializing variables
 		String text = input.nextLine();
 		
 		
@@ -13,24 +16,26 @@ public class J2 {
 		int sadCount = 0;
 		boolean none = false;
 		
-		
+		//using for loop to loop through array of input
 		for(int i = 0; i < characters.length; i++){
-			
+			//checking if character has ':' symbol and confirming it is over 3 characters
 			if(characters[i] == ':' && characters.length > 2){
-				
+				//proceeding to check two indexes after the ':' symbol to determine if face is
+				//happy ')' or sad '('
 				if(characters[i+2] == ')'){
 					happyCount++;
 				}else if(characters[i+2] == '('){
 					sadCount++;
 				}
-				
+				//accounting for situations where array is less then two characters
 			}else if (characters.length < 2){
 				none = true;
 			}
 	
 		}
 		
-			
+			//use if statements to print which count had a higher integer value based on
+			//additions made in ^for loop
 			if(happyCount == 0 && sadCount ==  0 || none == true){
 				System.out.println("none");
 			}else if(happyCount == sadCount && happyCount != 0){
